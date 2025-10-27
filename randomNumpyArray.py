9bf377cf39697e2b03fd047bf48bf7ce4a218325
Anushruti Mahato <240118848+Anushruti47@users.noreply.github.com>
2025-10-27 23:48:16 +0530

    updated

--- randomNumpyArray.py
+++ randomNumpyArray.py
@@ -1,8 +1,10 @@
 import numpy as np
 import random
 
+#Can produce floating values as we are using rand
 array1=np.random.rand(5)
 print(array1)
 
+#Produces integer values as we are using randint values
 random_array=np.random.randint(0,10,size=5)
 print(random_array)
