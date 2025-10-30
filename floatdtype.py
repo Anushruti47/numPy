22b42c112314e628f529d06605e22ad83eb4129f
Anushruti Mahato <240118848+Anushruti47@users.noreply.github.com>
2025-10-30 23:28:33 +0530

    floatdtype updated

---
+++ floatdtype.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+array_float=np.array([0.1,0.2,0.3,0.4,0.5],dtype=np.float64)
+print(array_float)
+
+array_complex=np.array([1+1j,2+2j,3+3j,4+4j],dtype=np.complex64)
+print(array_complex)
